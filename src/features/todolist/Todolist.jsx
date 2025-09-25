0f1e13f179ffa7d0b339238358818c4e5df5cc48
Umasri537 <gubbalaumasri4@gmail.com>
2025-09-25 15:22:49 +0530

    Todolist

--- src/features/todolist/Todolist.jsx
+++ src/features/todolist/Todolist.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo, deleteTodo } from './todolistSlice';
 
 function Todolist() {
     var {todos}=useSelector((state)=>state.todolistR);
-
+    var dispatch=useDispatch();
+    var [newtodo,setNewtodo]=React.useState("");
   return (
     <div className='border border-1 border-info-subtle m-2 p-2'>
       <h3>Todolist</h3>
+      <input type="text" onChange={(e)=>{setNewtodo(e.target.value)}}/>
+      <button onClick={()=>{dispatch(addTodo(newtodo))}}>AddTodo</button>
       <ul>
         {
             todos.map((todo)=>{
-                return<li>{todo}</li>
+                return<li>
+                    {todo}
+                    <button onClick={()=>{dispatch(deleteTodo())}}>Delete</button>
+                    </li>
             })
         }
       </ul>
